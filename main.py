282c4804a2960921b9ecd617177785900f7adf7a
TSOT7 <tommyma1234@gmail.com>
2024-12-09 16:58:21 -0500

    folder_creator() and file_sorter()

--- main.py
+++ main.py
@@ -4,14 +4,31 @@ import os
 # Define the Downloads folder path
 downloads_folder = Path.home() / "Downloads"
 
-img_path = r"C:\Users\tommy\Downloads\Imgs"
+# img_path = r"C:\Users\tommy\Downloads\Imgs"
+# zip_path = r"C:\Users\tommy\Downloads\Zips"
+# img_path = r"C:\Users\tommy\Downloads\Imgs"
+# img_path = r"C:\Users\tommy\Downloads\Imgs"
 
 # Creates Imgs Folder
-if not os.path.exists(img_path):
-    os.mkdir(img_path)
-    print(f"Folder '{img_path}' created successfully.")
-else:
-    print(f"Folder '{img_path}' already exists.")
+# if not os.path.exists(img_path):
+#     os.mkdir(img_path)
+#     print(f"Folder '{img_path}' created successfully.")
+# else:
+#     print(f"Folder '{img_path}' already exists.")
+
+def folder_creator(): 
+    folder_types = ["Imgs", "Zips", "MP3s", "MP4s"]
+    path_template = r"C:\Users\tommy\Downloads\\"
+    folder_dict = {}
+
+    for type in folder_types:
+        merged_path = path_template + type
+        if not os.path.exists(path_template + type):
+            os.mkdir(merged_path)
+            print(f"Folder {merged_path} created succesfully.")
+        else:
+            print(f"Folder {merged_path} already exists.")
+    
 
 # # declaring file types
 # png_files = list(downloads_folder.glob("*.png"))
@@ -20,13 +37,16 @@ else:
 # mp3_files = list(downloads_folder.glob("*.mp3"))
 # mp4_files = list(downloads_folder.glob("*.mp4"))
 
-file_types = ["png", "jpg", "zip", "mp3", "mp4"]
+def file_sorter():
 
-data_type_references = {}
+    file_types = ["png", "jpg", "zip", "mp3", "mp4"]
 
-for type in file_types:
-    data_type_references[f"{type}_files"] = list(downloads_folder.glob(f"*.{type}"))
+    data_type_references = {}
 
-for types in data_type_references:
-    print(types)
-    
+    for type in file_types:
+        data_type_references[f"{type}_files"] = list(downloads_folder.glob(f"*.{type}"))
+
+    for types in data_type_references:
+        print(types)
+
+folder_creator()
