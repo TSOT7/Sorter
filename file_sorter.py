4aacfde7c3a36da03c1664a7ba717630346bc27b
TSOT7 <tommyma1234@gmail.com>
2024-12-12 00:04:29 -0500

    Fixed sort_Files() in FileSorter

--- file_sorter.py
+++ file_sorter.py
@@ -21,10 +21,15 @@ class FileSorter:
         folder_path = self.__base_path
         for type in file_types:
             temp = list(folder_path.glob(f"*.{type}"))
-            if f"{type}_files" in data_type_references:
-                data_type_references[f"{type}_files"] = temp
-        else:
-            data_type_references[f"{type}_files"] = list(folder_path.glob(f"*.{type}"))
+            if type in data_type_references:
+                data_type_references[type] = temp
+            else:
+                data_type_references[type] = list(folder_path.glob(f"*.{type}"))
 
-        for types in data_type_references:
-            print(data_type_references[types])
+        for file_types, files in data_type_references.items():
+            print(f"\n{file_types.upper()} FILES:")
+            if files:
+                for file in files:
+                    print(file)
+            else:
+                print("  No files found.")
